75d44a543ea192d5bd37a87725200f6fee82d71c
Jordan Barrand <Jordan.barrand@outlook.com>
2021-03-09 16:39:19 +0000

    Adding game details

--- src/api.js
+++ src/api.js
@@ -37,3 +37,8 @@ const newGames = `games${api_key}&dates=${lastYear},${currentDate}&ordering=-rel
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
 export const newGamesURL = () => `${base_url}${newGames}`;
+
+//GAME DETAILS
+export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}${api_key}`;
+//Game Screenshots
+export const gameScreenshotURL = (game_id) => `${base_url}games/${game_id}/screenshots${api_key}`;
