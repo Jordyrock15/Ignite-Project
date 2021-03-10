3fd9a52b258f83fea04c9467dccc198fe403648e
Jordan Barrand <Jordan.barrand@outlook.com>
2021-03-10 19:45:21 +0000

    Adding the NavBar and Stars

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+//Router
 import { Route } from 'react-router-dom';
 //Components and pages
+import Nav from './components/Nav';
 import Home from './pages/Home';
+//Styles
 import GlobalStyles from './components/GlobalStyles';
 
 function App() {
 	return (
 		<div className='App'>
 			<GlobalStyles />
+			<Nav />
 			<Route path={['/game/:id', '/']}>
 				<Home />
 			</Route>
--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -5,6 +5,16 @@ import styled from 'styled-components';
 import { motion } from 'framer-motion';
 //Redux
 import { useSelector } from 'react-redux';
+//IMAGES
+import playstation from '../img/playstation.svg';
+import steam from '../img/steam.svg';
+import xbox from '../img/xbox.svg';
+import nintendo from '../img/nintendo.svg';
+import apple from '../img/apple.svg';
+import gamepad from '../img/gamepad.svg';
+//Star Images
+import starEmpty from '../img/star-empty.png';
+import starFull from '../img/star-full.png';
 
 const GameDetail = ({ pathID }) => {
 	const history = useHistory();
@@ -16,6 +26,39 @@ const GameDetail = ({ pathID }) => {
 			history.push('/');
 		}
 	};
+
+	//Get Stars
+	const getStars = () => {
+		const stars = [];
+		const rating = Math.floor(game.rating);
+		for (let i = 1; i <= 5; i++) {
+			if (i <= rating) {
+				stars.push(<img alt='star' key={i} src={starFull}></img>);
+			} else {
+				stars.push(<img alt='star' key={i} src={starEmpty}></img>);
+			}
+		}
+		return stars;
+	};
+
+	//GET PLATFORM IMAGES
+	const getPlatform = (platform) => {
+		switch (platform) {
+			case 'PlayStation 4':
+				return playstation;
+			case 'Xbox One':
+				return xbox;
+			case 'PC':
+				return steam;
+			case 'Nintendo Switch':
+				return nintendo;
+			case 'iOS':
+				return apple;
+			default:
+				return gamepad;
+		}
+	};
+
 	//Data
 	const { screen, game, isLoading } = useSelector((state) => state.detail);
 	return (
@@ -27,12 +70,17 @@ const GameDetail = ({ pathID }) => {
 							<div className='rating'>
 								<motion.h3 layoutId={`title ${pathID}`}>{game.name}</motion.h3>
 								<p>Rating: {game.rating}</p>
+								{getStars()}
 							</div>
 							<Info>
 								<h3>Platforms</h3>
 								<Platforms>
 									{game.platforms.map((data) => (
-										<h3 key={data.platform.id}>{data.platform.name}</h3>
+										<img
+											key={data.platform.id}
+											src={getPlatform(data.platform.name)}
+											alt={data.platform.name}
+										></img>
 									))}
 								</Platforms>
 							</Info>
@@ -71,7 +119,7 @@ const CardShadow = styled(motion.div)`
 	position: fixed;
 	top: 0;
 	left: 0;
-	z-index: 10;
+	z-index: 5;
 
 	&::-webkit-scrollbar {
 		width: 0.5rem;
@@ -94,6 +142,7 @@ const Detail = styled(motion.div)`
 	position: absolute;
 	left: 10%;
 	color: black;
+	z-index: 10;
 
 	img {
 		width: 100%;
@@ -104,6 +153,12 @@ const Stats = styled(motion.div)`
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
+
+	img {
+		width: 2rem;
+		height: 2rem;
+		display: inline;
+	}
 `;
 
 const Info = styled(motion.div)`
